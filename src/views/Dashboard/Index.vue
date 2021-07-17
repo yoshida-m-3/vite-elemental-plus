92fb366583ff8b4015268b3dd6e1384a68b2509e
yoshida-m-3 <milmed1013@gmail.com>
2021-07-17 21:58:59 +0900

    ドロワー追加

---
+++ src/views/Dashboard/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>ダッシュボードのインデックス</p>
+  <Drawer />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import Drawer from "./components/Drawer.vue";
+export default defineComponent({
+  name: 'Dashboard',
+  components: {
+    Drawer,
+  },
+})
+</script>
